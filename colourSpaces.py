7b8f4189299014c3d691186aca2be576ac9e16a4
VindictiveVeer <veersh4h@gmail.com>
2022-02-24 20:47:21 +0000

    Refactored code to sit within two objects, Image and ColourSpaces. Updated gamma correction method to not overflow

--- colourSpaces.py
+++ colourSpaces.py
@@ -1,61 +1,64 @@
 import numpy as np
 import cv2
 
-#read images in colour
-#returns numpy 3-dimensional array (matrix)
-def readColourImage(file):
-    return cv2.imread(file, cv2.IMREAD_COLOR)
+#class to handle anything related to acquiring and displaying image
+class Image:
+    #overriding constructor
+    #iscolour represents if image is colour (1) or black white (0)
+    def __init__(self,file, isColour=1):
+        self.file = file
+        self.img = cv2.imread(self.file, isColour) # matrix of vals
 
-#read image in greyscale
-def readBWImage(file):
-    return cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    #display image and title
+    def displayImage(self,title,img):
+        print("Press 0 to close window")
+        cv2.imshow(title,img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-#display image and title
-def displayImage(title,img):
-    print("Press 0 to close window")
-    cv2.imshow(title,img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#class to handle colour space transformations
+class ColourSpaces:
+    #extract BGR components
+    #returns unpacked var as blue, green, red
+    #usage: (b,g,r) = extractBGRComponents(img)
+    def extractBGRComponents(self,img):
+        return cv2.split(img)
 
-#extract BGR components
-#returns unpacked var as blue, green, red
-#usage: (b,g,r) = extractBGRComponents(img)
-def extractBGRComponents(img):
-    return cv2.split(img)
+    #merge individual BGR components
+    def mergeBGRComponents(self,b,g,r):
+        return cv2.merge((b,g,r))
 
-#merge individual BGR components
-def mergeBGRComponents(b,g,r):
-    return cv2.merge((b,g,r))
+    #convert RGB to Y'UV
+    def rgb2YUV(self,r,g,b):
+        #typecast to unsigned int
+        y = np.array(0.299*r+0.587*g+0.114*b, np.uint8)
+        u = np.array(b-y, np.uint8)
+        v = np.array(r-y, np.uint8)
+        return (y,u,v)
 
-#convert RGB to Y'UV
-def rgb2YUV(r,g,b):
-    #typecast to unsigned int
-    y = np.array(0.299*r+0.587*g+0.114*b, np.uint8)
-    u = np.array(b-y, np.uint8)
-    v = np.array(r-y, np.uint8)
-    return (y,u,v)
+    #convert Y'UV to Y'PbPr
+    def yuv2YPbPr(self,y,u,v):
+        pb = np.array((0.5/(1-0.114))*u, np.uint8)
+        pr = np.array((0.5/(1-0.299))*v, np.uint8)
+        return (y,pb,pr)
 
-#convert Y'UV to Y'PbPr
-def yuv2YPbPr(y,u,v):
-    pb = np.array((0.5/(1-0.114))*u, np.uint8)
-    pr = np.array((0.5/(1-0.299))*v, np.uint8)
-    return (y,pb,pr)
+    #convert Y'PbPr to Y'CbCr
+    def ypbpr2YCbCr(self,y,pb,pr):
+        y = 16+219*y
+        cb = 128+224*pb
+        cr = 128+224*pr
+        return (y,cb,cr)
 
-#convert Y'PbPr to Y'CbCr
-def ypbpr2YCbCr(y,pb,pr):
-    y = 16+219*y
-    cb = 128+224*pb
-    cr = 128+224*pr
-    return (y,cb,cr)
+    #gamma correction
+    def gammaCorrection(self,img,gamma):
+        img = img.astype("double") #typecast to not lose precision
+        imgPrime = np.clip(255 * ((img/255) ** 1/gamma),0,255) #prevent overflow
+        return imgPrime.astype("uint8") #typecast back so can be displayed
 
-#gamma correction
-def gammaCorrection(y,gamma):
-    return np.array(y**gamma,np.float)
 
-file = "lena512color.tiff"
-img = readColourImage(file)
-(b,g,r) = extractBGRComponents(img)
-(y,u,v) = rgb2YUV(r,g,b)
-vout = gammaCorrection(y,2)
-print(vout)
-displayImage("gamma correction",vout)
+lena = Image("lena512color.tiff",1)
+test = ColourSpaces()
+(b,g,r) = test.extractBGRComponents(lena.img)
+(y,u,v) = test.rgb2YUV(r,g,b)
+vout = test.gammaCorrection(lena.img,5)
+lena.displayImage("gamma correction",vout)
